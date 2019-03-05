970b941d946b047fb2b73f839364480bdd7b81b6
poushene <3881854@gmail.com>
2019-03-05 15:15:21 +0300

    New copy

---
+++ Goodbye.js
@@ -0,0 +1,18 @@
+const smallestDivisor = (num) => {
+    if (num === 1) {
+        return 1
+    };
+    if (num > 0) {
+        const iter = (divisor, acc) => {
+            if (acc % divisor === 0) {
+                console.log("smallest divisor is " + divisor)
+                return divisor
+            };
+            console.log(divisor)
+            return iter(divisor + 1, num)
+        };
+        iter(2, num)
+    };
+    console.log("Please, enter an argument wich is > 0")
+};
+smallestDivisor(-10)
