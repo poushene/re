2345c3f1c36d03f497d5c9ec2945e9c42bb4d45d
poushene <3881854@gmail.com>
2019-03-05 14:58:24 +0300

    More good things done

---
+++ SD.js
@@ -0,0 +1,39 @@
+const stringReverse = (str) => {
+    let newString = str[str.length - 1]
+    while (str.length !== newString.length) {
+        newString = newString + str[str.length - newString.length - 1]
+    }
+    return newString
+};
+
+const isPrime = (num) => {
+    if (num <= 1) {
+        console.log('false')
+        return false
+    };
+    for (let check = 2; check < num - 1; check = check + 1) {
+        if (num % check === 0) {
+            console.log('false')
+            return false
+        };
+    };
+    console.log('true')
+    return true
+};
+isPrime(4)
+
+const square = (n) => {
+    return n ** 2
+}
+console.log(square(12))
+
+const sumOfSquares = (n) => {
+    return square(n) * 2
+}
+
+console.log(sumOfSquares(12))
+
+const squareSumOfSquares = (n) => {
+    return sumOfSquares(num) ** 2
+}
+console.log(squareSumOfSquares())
